d4ef6b910ecd40f4c053a281b81251c8ef5cf22a
Shishir Agrawal <11269129+sagrawalx@users.noreply.github.com>
2023-12-10 19:43:48 -0800

    add helper functions

--- source/sim-plot.R
+++ source/sim-plot.R
@@ -1,3 +1,5 @@
+# This script file generates plots of the results produced by sim-run.csv.
+
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 source("base.R")
 library(colorspace)
@@ -21,7 +23,6 @@ results <- read_csv(results_file)
 # ==============================================================================
 
 theme_set(theme_minimal())
-
 short_names <- c("mle", 
                  "heuristic", 
                  "asy_main", 
@@ -39,40 +40,48 @@ alphas <- c(1, 0.5, rep(1, 5))
 # Takes as input a "true" vector, ie, a vector of the true DIF status of some
 # tables, and a "result" vector which indicates the output of some method
 # of DIF analysis. Returns a logical vector which indicates whether the method
-# succeeded at correctly *detecting* DIF. 
-detect <- function(true, result) {
-    map2_lgl(true, 
-             result, 
-             \(x, y) if (x == dif_test_results$none) { y == dif_test_results$none } 
-             else { y %in% c(dif_test_results$uniform, 
-                             dif_test_results$nonuniform, 
-                             dif_test_results$unclassifiable) })
+# succeeded at correctly *detecting* DIF. We first define a helper function, 
+# and then a vectorized version.
+detect_helper <- function(true, result) {
+    ifelse(true == dif_test_results$none, 
+           result == dif_test_results$none, 
+           result %in% c(dif_test_results$uniform, 
+                         dif_test_results$nonuniform, 
+                         dif_test_results$unclassifiable))
 }
 
-# Like above, but now returns a logical vector which indicates whether the 
-# method succeeded at correctly *classifying* DIF. 
+detect <- function(true, result) {
+    map2_lgl(true, result, detect_helper)
+}
+
+# Like above, but now returns a logical vector which indicates whether the
+# method succeeded at correctly *classifying* DIF. Again, we first define a
+# helper function, and then a vectorized version.
+classify_helper <- function(true, result) {
+    ifelse(true == dif_test_results$none, NA, true == result)
+}
+
 classify <- function(true, result) {
-    map2_lgl(true, result, 
-             \(x, y) if (x == dif_test_results$none) { NA } else { x == y })
+    map2_lgl(true, result, classify_helper)
 }
 
 # For labeling plots
-step_labels <- c(detect = "Detect",
+step_labels <- c(detect = "Detect", 
                  classify = "Classify")
-type_labels <- c(none = "No DIF",
-                 unif = "Uniform DIF",
+type_labels <- c(none = "No DIF", 
+                 unif = "Uniform DIF", 
                  nonunif = "Nonuniform DIF")
-norm_labels <- c("0" = "Norm 0",
-                 "1" = "Norm 1",
+norm_labels <- c("0" = "Norm 0", 
+                 "1" = "Norm 1", 
                  "2" = "Norm 2")
-thing_labels <- c(mle = "MLE Exists",
-                  heuristic = "Heuristic Passes",
-                  asy_main = "Asymptotic Succeeds",
-                  exc_main = "Exact Succeeds",
-                  asy_swap = "Swapped Asymptotic Succeeds",
-                  exc_swap = "Swapped Exact Succeeds",
+thing_labels <- c(mle = "MLE Exists", 
+                  heuristic = "Heuristic Passes", 
+                  asy_main = "Asymptotic Succeeds", 
+                  exc_main = "Exact Succeeds", 
+                  asy_swap = "Swapped Asymptotic Succeeds", 
+                  exc_swap = "Swapped Exact Succeeds", 
                   asy_comp = "Asymptotic with Comparison Succeeds")
-family_labels <- c(llm = "Log-Linear Models",
+family_labels <- c(llm = "Log-Linear Models", 
                    lrm = "Logistic Regressions")
 
 names(colors) <- map_chr(short_names, \(x) thing_labels[[x]])
@@ -80,9 +89,9 @@ names(alphas) <- map_chr(short_names, \(x) thing_labels[[x]])
 
 # For naming plots
 main_method_pair <- c("asy_main", "exc_main")
-method_pairs <- list(main_method_pair,
-                     c("asy_main", "asy_swap"),
-                     c("exc_main", "exc_swap"),
+method_pairs <- list(main_method_pair, 
+                     c("asy_main", "asy_swap"), 
+                     c("exc_main", "exc_swap"), 
                      c("asy_main", "asy_comp"))
 
 method_pair_suffix <- function(x) {
@@ -111,7 +120,7 @@ filename <- function(s,
 
 plot <- function(s, 
                  family, 
-                 methods = main_method_pair,
+                 methods = main_method_pair, 
                  filename = NULL) {
     # Return empty string if requesting LRM plot for non-dichotomous seed
     if (family == "lrm" && !models[[match(s, seeds)]]$is_dichotomous) {
@@ -128,9 +137,9 @@ plot <- function(s,
         filter(seed == s) |> 
         select(type, norm, sample_size, 
                detect_mle = mle_cols[1], 
-               classify_mle = mle_cols[2],
-               detect_heuristic = heuristic_cols[1],
-               classify_heuristic = heuristic_cols[2],
+               classify_mle = mle_cols[2], 
+               detect_heuristic = heuristic_cols[1], 
+               classify_heuristic = heuristic_cols[2], 
                all_of(method_cols))
     
     # Compare results
@@ -153,9 +162,9 @@ plot <- function(s,
                      values_to = "proportion") |> 
         filter(!is.na(proportion)) |> 
         ungroup() |> 
-        mutate(step = map_chr(thing, \(x) 
-                              if (str_detect(x, "detect_")) { "detect" } 
-                              else { "classify" })) |> 
+        mutate(step = map_chr(thing, \(x) ifelse(str_detect(x, "detect_"), 
+                                                 "detect", 
+                                                 "classify"))) |> 
         mutate(thing = str_replace(thing, "detect_", "")) |> 
         mutate(thing = str_replace(thing, "classify_", "")) |> 
         mutate(thing = str_replace(thing, str_c(family, "_"), ""))
@@ -170,36 +179,38 @@ plot <- function(s,
     df <- df |> 
         mutate(type = factor(type, 
                              levels = names(type_labels), 
-                             labels = unname(type_labels),
+                             labels = unname(type_labels), 
                              ordered = TRUE)) |> 
         mutate(norm = factor(norm, 
                              levels = names(norm_labels), 
-                             labels = unname(norm_labels),
+                             labels = unname(norm_labels), 
                              ordered = TRUE)) |> 
         mutate(thing = factor(thing, 
                               levels = names(thing_labels), 
-                              labels = unname(thing_labels),
+                              labels = unname(thing_labels), 
                               ordered = TRUE)) |> 
         mutate(step = factor(step, 
                              levels = names(step_labels), 
-                             labels = unname(step_labels),
+                             labels = unname(step_labels), 
                              ordered = TRUE))
     
-    # Make plot    
+    # Make plot
     out <- df |> 
         ggplot(aes(x = sample_size, 
                    y = proportion, 
-                   color = thing,
+                   color = thing, 
                    alpha = thing)) + 
         facet_grid(step ~ type + norm, 
-                   labeller = labeller(Helper = function(x) { rep("", length(x)) })) + 
-        geom_line() +
-        geom_hline(yintercept = 0.95, linetype = "dotted", color="lightgray") + 
+                   labeller = labeller(Helper = \(x) rep("", length(x)))) + 
+        geom_line() + 
+        geom_hline(yintercept = 0.95, 
+                   linetype = "dotted", 
+                   color = "lightgray") + 
         expand_limits(y = c(0, 1)) + 
         scale_x_continuous(breaks = c(0, 1000, 2000)) + 
         scale_colour_manual(values = colors, name = "") + 
         scale_alpha_manual(values = alphas, name = "") + 
-        theme(legend.position="bottom") + 
+        theme(legend.position = "bottom") + 
         xlab("Sample Size") + 
         ylab("Proportion") + 
         ggtitle(str_c(family_labels[[family]], ", Seed ", s))
@@ -210,7 +221,7 @@ plot <- function(s,
     } else {
         ggsave(filename, out, 
                width = 10, height = 6, units = "in", 
-               bg = 'white')
+               bg = "white")
         return(filename)
     }
 }
@@ -232,7 +243,7 @@ paper_folder <- "../paper/"
 
 tibble(seed = c(1947, 1947, 1948, 1930), 
        family = c("llm", "lrm", "llm", "lrm")) |> 
-    mutate(filename = map2_chr(seed, family,
+    mutate(filename = map2_chr(seed, family, 
                                \(x, y) filename(x, y, format = "pdf"))) |> 
     mutate(filename = str_c(paper_folder, filename)) |> 
     mutate(filename = pmap_chr(list(seed, family, filename), 
@@ -245,8 +256,8 @@ tibble(seed = c(1947, 1947, 1948, 1930),
 fig_folder <- "../figs/"
 
 grid <- expand_grid(seed = seeds, 
-            family = c("llm", "lrm"),
-            method_pair = method_pairs) |> 
+                    family = c("llm", "lrm"), 
+                    method_pair = method_pairs) |> 
     mutate(filename = pmap_chr(list(seed, family, method_pair), filename)) |> 
     mutate(filename = str_c(fig_folder, filename)) |> 
     mutate(filename = pmap_chr(list(seed, family, method_pair, filename), 
@@ -270,15 +281,15 @@ cell_maker <- function(filename, img_width) {
         return("")
     }
     x <- str_replace(filename, "../", "")
-    str_c("<img width=\"", img_width ,"\" src=\"./", x, "\" />")
+    str_c("<img width=\"", img_width, "\" src=\"./", x, "\" />")
 }
 
 # ==============================================================================
 # README Table for Primary Comparison Plots
 # ==============================================================================
 
-titles <- c("Seed",
-            "Log-Linear Models",
+titles <- c("Seed", 
+            "Log-Linear Models", 
             "Logistic Regressions")
 
 primary <- grid |> 
@@ -288,12 +299,12 @@ primary <- grid |>
     rename_with(\(.) titles)
 
 primary_output <- primary |> 
-    xtable(align = rep("l", ncol(primary)+1)) |> 
-    print(type = "html",
-          include.rownames = FALSE,
-          sanitize.text.function = identity,
-          html.table.attributes = "",
-          comment = FALSE,
+    xtable(align = rep("l", ncol(primary) + 1)) |> 
+    print(type = "html", 
+          include.rownames = FALSE, 
+          sanitize.text.function = identity, 
+          html.table.attributes = "", 
+          comment = FALSE, 
           print.results = FALSE) |> 
     str_replace_all("\n  ", "\n") |> 
     str_replace_all("\n ", "\n") |> 
@@ -308,12 +319,12 @@ cat(primary_output)
 # README Table for Secondary Comparison Plots
 # ==============================================================================
 
-titles <- c("Seed",
-            "LLM Asy Swap",
-            "LLM Exc Swap",
-            "LLM Asy Comp",
-            "LRM Asy Swap",
-            "LRM Exc Swap",
+titles <- c("Seed", 
+            "LLM Asy Swap", 
+            "LLM Exc Swap", 
+            "LLM Asy Comp", 
+            "LRM Asy Swap", 
+            "LRM Exc Swap", 
             "LRM Asy Comp")
 
 secondary <- grid |> 
@@ -322,23 +333,23 @@ secondary <- grid |>
     pivot_wider(names_from = name, values_from = filename) |> 
     rename_with(\(.) titles)
 
-header <- str_c("<tr> ",
-                "<th rowspan=2> Seed </th> ",
-                "<th colspan=3> Log-Linear Models </th> ",
-                "<th colspan=3> Logistic Regressions </th> ",
+header <- str_c("<tr> ", 
+                "<th rowspan=2> Seed </th> ", 
+                "<th colspan=3> Log-Linear Models </th> ", 
+                "<th colspan=3> Logistic Regressions </th> ", 
                 "</tr>\n")
 
 secondary_output <- secondary |> 
-    xtable(align = rep("l", ncol(secondary)+1)) |> 
-    print(type = "html",
-          include.rownames = FALSE,
-          sanitize.text.function = identity,
-          html.table.attributes = "",
-          comment = FALSE,
+    xtable(align = rep("l", ncol(secondary) + 1)) |> 
+    print(type = "html", 
+          include.rownames = FALSE, 
+          sanitize.text.function = identity, 
+          html.table.attributes = "", 
+          comment = FALSE, 
           print.results = FALSE) |> 
     str_replace_all("\n  ", "\n") |> 
     str_replace_all("\n ", "\n") |> 
-    str_replace("<table >\n", str_c("<table>\n", header)) |>
+    str_replace("<table >\n", str_c("<table>\n", header)) |> 
     str_replace("<th> Seed </th>", "") |> 
     str_replace_all("LLM ", "") |> 
     str_replace_all("LRM ", "") |> 
